e1864e049752e17f093008e161d041ca6b1638e1
ajdimarvellousgo@gmail.com <ajdimarvellousgo@gmail.com>
2022-06-23 16:16:16 +0100

    Add project files.

---
+++ Script1.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+console.log("My Name:Ajadi Marvellous Oladayo");
+console.log("My Height: 5'7ft");
+console.log("My Country: Nigeria");
+
+alert("My Name:Ajadi Marvellous Oladayo");
+alert("My Height: 5'7ft");
+alert("My Country: Nigeria");
+
+var username = prompt("What is your name?");
+var height = prompt("What is your Height?")
+var Country = prompt("Height?")
+alert(username)
+alert(height)
+alert(Country)
